f52015524bc348b3cc14aededc11d3e2e4bd92c5
dv-15 <143259664+dv-15@users.noreply.github.com>
2023-09-01 16:48:53 +0530

    customer detail

---
+++ customer.py
@@ -0,0 +1,3 @@
+class Customer:
+    def customer_detail(self,name,address,pincode,email):
+        print(f"customerName :{name},Address is : {address} , pincode is : {pincode} , email is : {email}")
