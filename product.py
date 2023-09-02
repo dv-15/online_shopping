6af076cce8b8bc9561863d2c9056010fc1479c68
dv-15 <143259664+dv-15@users.noreply.github.com>
2023-09-02 22:39:26 +0530

    remove a file

--- product.py
+++
@@ -1,5 +0,0 @@
-class Product:
-    def __init__(self,product_name,price):
-        self.product_name = product_name
-        self.price = price
-        print(f"product_Name : {self.product_name} , price : {self.price}")
